ecce2a0ab45bf079607fa28d9b02cb918763efd9
Indexal <148807519+Index-al@users.noreply.github.com>
2024-04-26 19:43:13 -0500

    Revert "remove card from deck functionality added"

--- public/js/remove.js
+++ public/js/remove.js
@@ -20,20 +20,3 @@ const removeButtons = document.querySelectorAll(".remove");
 removeButtons.forEach((button) => {
   button.addEventListener("click", removeFromCollection);
 });
-
-async function removeFromDeck(e) {
-  e.preventDefault();
-  const key_id = e.target.getAttribute("card-id");
-  const response = await fetch("/api/deck-builder/update", {
-    method: "DELETE",
-    body: JSON.stringify({ key_id }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  if (response.ok) {
-    document.location.reload();
-  } else {
-    await Toast.fire("Failed to remove Card from Deck");
-  }
-};
